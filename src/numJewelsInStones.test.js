d21a4928d37ef3cd0c0d93ebe1213b278a1c11d6
vpolets <vpolets@gmail.com>
2019-06-12 17:18:05 +0300

    added new tests

--- src/numJewelsInStones.test.js
+++ src/numJewelsInStones.test.js
@@ -7,7 +7,37 @@ test('Should ignore chars in another case', () => {
     .toBe(0);
 });
 
+test('Should ignore another symbols', () => {
+  expect(calculate('z123!_', 'ZZZ!ZZZ123_1'))
+    .toBe(0);
+});
+
+test('Should ignore double symbols in jewel string', () => {
+  expect(calculate('zzzzZZZZZZZZ', 'zzzZZZZ'))
+    .toBe(7);
+});
+
 test('Should return correct count of jewels in stones', () => {
   expect(calculate('zZA', 'ZaZaAAazzzz'))
     .toBe(8);
+});
+
+test('Should return zero for empty string stones', () => {
+  expect(calculate('zZA', ''))
+    .toBe(0);
+});
+
+test('Should return zero for empty string jewels', () => {
+  expect(calculate('', 'AAaaLllDdoooo'))
+    .toBe(0);
+});
+
+test('Should return zero for empty strings jewels and stones', () => {
+  expect(calculate('', ''))
+    .toBe(0);
+});
+
+test('Should not return zero for empty string jewels and stones', () => {
+  expect(calculate('', ''))
+    .toBe(0);
 });
